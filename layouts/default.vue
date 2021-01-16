80c2fbb3148d47d91e8b83c0b6318f98afa5471b
Stephen Studer <stephenkstuder@gmail.com>
2021-01-16 15:25:28 -0700

    added some meta stuff for sharing

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,30 @@
   </div>
 </template>
 
+<script>
+import ogImage from '../assets/ogImage.png'
+export default {
+  head() {
+    return {
+      titleTemplate: 'Pulp Fantasy Handicap Helper',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Enter your handicap/allstar status and see the point potientials for your rider!',
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: ogImage,
+        },
+      ],
+    }
+  },
+}
+</script>
+
 <style>
 .bg-dark {
   background: #2a2a45;

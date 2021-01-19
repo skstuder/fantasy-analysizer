96ae96b311d639816429e3dd3281f9136cf621e8
Stephen Studer <stephenkstuder@gmail.com>
2021-01-18 20:52:31 -0700

    updated commit and favicon icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.jpg' },
+      { rel: 'icon', type: 'image/x-icon', href: 'static/weege.png' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       {
         rel: 'stylesheet',

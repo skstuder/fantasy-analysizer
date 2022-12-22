49e122f27d45113e7d7f5e64b0ec5a31ce66ed45
Makenna Studer <makennastuder@gmail.com>
2022-12-22 16:18:21 -0700

    update empty state

--- components/NoResults.vue
+++ components/NoResults.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-col justify-center items-center">
     <h1 class="text-xl lg:text-3xl font-weight-bold mb-5">
-      You Have to Enter a Handicap
+      Steve can't find your handicap, enter it above
     </h1>
     <img
       class="md:w-1/2"
-      src="../assets/do-something.gif"
-      alt="stick figure poking the ground with a stick"
+      src="../assets/steve-confused.gif"
+      alt="steve matthes wandering around confused"
     />
   </div>
 </template>
